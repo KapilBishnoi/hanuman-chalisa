0ef6e943df8f59c089a25815b6e5bab9a377351d
KapilBishnoi <KapilBishnoi@gmail.com>
2020-04-13 00:46:22 -0700

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,3 @@
+import React from "react"
+
+export default () => <h1>श्री हनुमान चालीसा</h1>
---
+++ src/pages/index.js
@@ -0,0 +1,28 @@
+import React from "react"
+import Header from "../components/header"
+import styles from "./index.module.css"
+import ColumnOne from "../components/column-one"
+import ColumnTwo from "../components/column-two"
+import ColumnThree from "../components/column-three"
+import ColumnFour from "../components/column-four"
+
+export default () => <div>
+     <div className={styles.header} >  <Header /> </div>
+     <div>
+   <p>  श्रीगुरु चरन सरोज रज निज मनु मुकुरु सुधारि । </p>
+ <p>बरनउँ रघुबर बिमल जसु जो दायकु फल चारि ॥ </p>
+
+<p>बुद्धिहीन तनु जानिके, सुमिरौं पवन कुमार</p>
+<p>बल बुधि विद्या देहु मोहि, हरहु कलेश विकार</p>
+     </div>
+    <div className={styles.container}>
+       <div>  <ColumnOne /> </div>
+       <div>  <ColumnTwo /> </div>
+       <div> <ColumnThree /> </div>
+       <div> <ColumnFour /> </div>
+    </div>
+    <div>
+  <p>  पवन तनय संकट हरन, मंगल मूरति रूप। </p>
+<p>राम लखन सीता सहित, हृदय बसहु सुर भूप॥ </p>
+    </div>
+    </div>
